5d95b31f2256f681c65c8a5a7e0575ab960dbe4e
Michelle <michelleh11219@gmail.com>
2024-05-04 16:12:00 -0400

    add Notification to WelcomePage

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -1,12 +1,33 @@
-import '../stylesheets/App.css';
+import '../stylesheets/WelcomePage.css';
+import { useState } from 'react';
+import axios from 'axios';
+import Notification from './Notification.js';
 
 export default function WelcomePage(props) {
   const changeActive = props.changeActive;
+  const [showNotif, setShowNotif] = useState(false);
+  const closeNotif = () => {
+    setShowNotif(false);
+  }
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+  axios.get("http://localhost:8000/getLoggedIn")
+    .then(response => {
+      setIsLoggedIn(response.data.loggedIn);
+      setShowNotif(response.data.loggedIn);
+    })
+    .catch(error => {
+      console.error('Error getting logged in', error);
+    });
   return (
+    <>
     <div id="welcome">
-      <button onClick={() => {changeActive("Register")}}>Register</button>
-      <button onClick={() => {changeActive("Login")}}>Login</button>
-      <button>Guest</button>
+      <button onClick={() => {changeActive("Register")}} disabled = {isLoggedIn} >Register</button>
+      <button onClick={() => {changeActive("Login")}} disabled = {isLoggedIn} >Login</button>
+      <button disabled = {isLoggedIn} >Guest</button>
     </div>
+    {showNotif && 
+      <Notification message="You are already logged in" onClose={closeNotif}/>
+    }
+    </>
   );
 }
